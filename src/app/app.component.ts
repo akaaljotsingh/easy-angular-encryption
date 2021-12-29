7acf992d1795efed7c7219b27df100e164c45b38
aksingh <akaaljotsingh@gmail.com>
2021-12-29 20:55:07 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
-
+import { MatSnackBar } from '@angular/material/snack-bar';
+import * as CryptoJS from 'crypto-js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'secretMsgs';
+  tokenFromUI: string = "0123456789123456";
+  encrypted: any = "";
+  decrypted: any;
+
+  request: any;
+  response: any;
+  requestPass:any;
+  responsePass:any;
+  outPass: any;
+  toggleFlag: boolean = false;
+
+  constructor(private _snackBar: MatSnackBar){}
+
+  encryptUsingAES256() {
+    this.decrypted = "";
+    let space = "#";
+    // console.log(this.request.concat(" "+this.requestPass));
+    let req = this.request.concat(space+this.requestPass)
+    let _key = CryptoJS.enc.Utf8.parse(this.tokenFromUI);
+    let _iv = CryptoJS.enc.Utf8.parse(this.tokenFromUI);
+    let encrypted = CryptoJS.AES.encrypt(
+      JSON.stringify(req), _key, {
+        keySize: 16,
+        iv: _iv,
+        mode: CryptoJS.mode.ECB,
+        padding: CryptoJS.pad.Pkcs7
+      });
+    this.encrypted = encrypted.toString();
+  }
+  decryptUsingAES256() {
+    let _key = CryptoJS.enc.Utf8.parse(this.tokenFromUI);
+    let _iv = CryptoJS.enc.Utf8.parse(this.tokenFromUI);
+
+    let a = CryptoJS.AES.decrypt(
+      this.response, _key, {
+        keySize: 16,
+        iv: _iv,
+        mode: CryptoJS.mode.ECB,
+        padding: CryptoJS.pad.Pkcs7
+      }).toString(CryptoJS.enc.Utf8);
+      a = a.slice(1,-1);
+      let b = a.split('#');
+      this.outPass = b[1];
+      if(this.outPass == this.responsePass) {
+        this.decrypted = b[0];
+      } else {
+        this.decrypted = '';
+        this._snackBar.open('Wrong Password', "Okay", {
+          duration: 3000,
+        });
+      }
+  }
+  copyToClipboard() {
+    this._snackBar.open('Text copied to clipboard', "Okay", {
+      duration: 3000,
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatTabsModule} from '@angular/material/tabs';
+import { FormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {ClipboardModule} from '@angular/cdk/clipboard';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +22,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatTabsModule,
+    FormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSnackBarModule,
+    ClipboardModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
